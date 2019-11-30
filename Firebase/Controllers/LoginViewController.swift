c372a3b308ee24c358c903b790962c51a45247a9
wtildestar <31348327+wtildestar@users.noreply.github.com>
2019-11-30 10:51:09 +0500

    Preparing TasksViewController

---
+++ Firebase/Controllers/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Firebase
+//
+//  Created by wtildestar on 30/11/2019.
+//  Copyright © 2019 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var warnLabel: UILabel!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func loginTapped(_ sender: Any) {
+        
+    }
+    @IBAction func registerTapped(_ sender: Any) {
+        
+    }
+}
+
---
+++ Firebase/Controllers/TasksViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TasksViewController.swift
+//  Firebase
+//
+//  Created by wtildestar on 30/11/2019.
+//  Copyright © 2019 wtildestar. All rights reserved.
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .clear
+        cell.textLabel?.text = "This is cell number \(indexPath.row)"
+        cell.textLabel?.textColor = .white
+        return cell
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func addTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
